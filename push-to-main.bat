a58706de14107809fae54d8e96344e89b03be4f4
official-notfishvr <notfishvrvr+6545@gmail.com>
2025-07-16 18:59:42 -0500

    v3 beta - new categories and more

--- push-to-main.bat
+++ push-to-main.bat
@@ -7,8 +7,8 @@ git update-index --assume-unchanged push-to-dev.bat
 git add .
 git reset push-to-dev.bat
 git commit -m "v3 beta - new categories and more"
-git branch -M dev
-git push origin dev --force
+git branch -M main
+git push origin main --force
 
 REM Optionally reset the assume-unchanged flag if needed
 REM git update-index --no-assume-unchanged push-to-dev.bat
